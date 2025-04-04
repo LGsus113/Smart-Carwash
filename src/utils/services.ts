5279a24ce4c9ed6827bc3289f871f914733bce57
LGsus113 <weikgoggowb@gmail.com>
2025-04-03 21:45:17 -0500

    Added service page

---
+++ src/utils/services.ts
@@ -0,0 +1,58 @@
+import Roller from "@src/assets/icon-svg/roller.svg";
+import Security from "@src/assets/icon-svg/security.svg";
+import Spray from "@src/assets/icon-svg/spray.svg";
+import Diamond from "@src/assets/icon-svg/diamond.svg";
+import Car from "@src/assets/icon-svg/car.svg";
+import Tool from "@src/assets/icon-svg/tool.svg";
+
+import Tapiceria from "@public/galeria-services/cambiar-tapiceria-coche.webp";
+import Descontaminacion from "@public/galeria-services/limpia-llantas-ferrico.webp";
+import Mecanica from "@public/galeria-services/mecanica.webp";
+import Pintura from "@public/galeria-services/Planchado-y-pintura.webp";
+import Polarizado from "@public/galeria-services/Polarizados-nanoceramica.webp";
+import Pulido from "@public/galeria-services/pulir-auto-scaled.webp";
+
+export const SERVICES_DATA = [
+  {
+    icon: Roller,
+    imageLink: Pintura.src,
+    title: "Planchado y Pintura",
+    description: "Reparación profesional de abolladuras y pintura",
+    link: "#",
+  },
+  {
+    icon: Security,
+    imageLink: Polarizado.src,
+    title: "Polarizado",
+    description: "Proteción con películas de alta calidad",
+    link: "#",
+  },
+  {
+    icon: Spray,
+    imageLink: Tapiceria.src,
+    title: "Tapizado y Limpieza",
+    description: "Limpieza profunda del interior",
+    link: "#",
+  },
+  {
+    icon: Diamond,
+    imageLink: Pulido.src,
+    title: "Pulido y Abrillantado",
+    description: "Acabado brillante y protección duradera",
+    link: "#",
+  },
+  {
+    icon: Car,
+    imageLink: Descontaminacion.src,
+    title: "Descontaminación",
+    description: "Eliminación de residuos metálicos",
+    link: "#",
+  },
+  {
+    icon: Tool,
+    imageLink: Mecanica.src,
+    title: "Mecánica Básica",
+    description: "Mantenimiento preventivo",
+    link: "#",
+  },
+] as const;
