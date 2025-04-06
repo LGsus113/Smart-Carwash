ebf6f59a8340709fd73d364ff90af98963ae6a77
LGsus113 <weikgoggowb@gmail.com>
2025-04-06 13:15:40 -0500

    added info card and ubication

---
+++ src/utils/icon-contact-list.ts
@@ -0,0 +1,33 @@
+import FB from "@src/assets/redes-svg/fb.svg";
+import Email from "@src/assets/redes-svg/email.svg";
+import Instagram from "@src/assets/redes-svg/instagram.svg";
+import Phone from "@src/assets/redes-svg/phone.svg";
+import Ubication from "@src/assets/redes-svg/ubication.svg";
+
+export const CONTACT_INFO = [
+  {
+    icon: Ubication,
+    label: "Sibelius 1049, Trujillo, Peru",
+  },
+  {
+    icon: Phone,
+    label: "+51 913 834 349",
+  },
+  {
+    icon: Email,
+    label: "smartcarwashdetailing@gmail.com",
+  },
+];
+
+export const SOCIAL_MEDIA = [
+  {
+    icon: FB,
+    label: "Facebook",
+    link: "https://www.facebook.com/share/15xeSc1J6U/",
+  },
+  {
+    icon: Instagram,
+    label: "Instagram",
+    link: "https://www.instagram.com/smartcardetailing.pe/?fbclid=IwY2xjawJfo4FleHRuA2FlbQIxMAABHv9BRTjSXfaBVTq7OOxlQWt68KM_c6ZcO9eJbZFeszDTTnzUh---uHBC2pZ-_aem_3bT_YGNwORSf8R7TaRNSeQ#",
+  },
+];
