df836026987a21598d702a396a15d8a1d0027b8a
LGsus113 <weikgoggowb@gmail.com>
2025-04-09 20:28:09 -0500

    New files and icons have been added for use in the page's modal component. This component is believed to be for the page's modal component.

--- src/utils/icon-contact-list.ts
+++ src/utils/icon-contact-list.ts
@@ -1,6 +1,7 @@
 import FB from "@src/assets/redes-svg/fb.svg";
 import Email from "@src/assets/redes-svg/email.svg";
 import Instagram from "@src/assets/redes-svg/instagram.svg";
+import Tiktok from "@src/assets/redes-svg/tiktok.svg";
 import Phone from "@src/assets/redes-svg/phone.svg";
 import Ubication from "@src/assets/redes-svg/ubication.svg";
 
@@ -29,5 +30,10 @@ export const SOCIAL_MEDIA = [
     icon: Instagram,
     label: "Instagram",
     link: "https://www.instagram.com/smartcardetailing.pe/?fbclid=IwY2xjawJfo4FleHRuA2FlbQIxMAABHv9BRTjSXfaBVTq7OOxlQWt68KM_c6ZcO9eJbZFeszDTTnzUh---uHBC2pZ-_aem_3bT_YGNwORSf8R7TaRNSeQ#",
+  },
+  {
+    icon: Tiktok,
+    label: "Tiktok",
+    link: "#",
   },
 ];
